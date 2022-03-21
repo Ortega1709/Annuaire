5b847a90bce50695176ba62fe74045739176123c
kabweortega@gmail.com <kabweortega@gmail.com>
2022-03-21 14:37:31 +0200

    Initial commit

--- src/classes/Adresse.java
+++ src/classes/Adresse.java
@@ -1,4 +1,48 @@
 package classes;
 
 public class Adresse {
+    private int numeroAvenue;
+    private String nomAvenue;
+    private String nomProvince;
+    private String nomPays;
+
+    public Adresse(int numeroAvenue, String nomAvenue, String nomProvince, String nomPays){
+        this.numeroAvenue = numeroAvenue;
+        this.nomAvenue = nomAvenue;
+        this.nomProvince = nomProvince;
+        this.nomPays = nomPays;
+    }
+
+    /* getters and setters */
+    public int getNumeroAvenue() {
+        return numeroAvenue;
+    }
+
+    public void setNumeroAvenue(int numeroAvenue) {
+        this.numeroAvenue = numeroAvenue;
+    }
+
+    public String getNomAvenue() {
+        return nomAvenue;
+    }
+
+    public void setNomAvenue(String nomAvenue) {
+        this.nomAvenue = nomAvenue;
+    }
+
+    public String getNomProvince() {
+        return nomProvince;
+    }
+
+    public void setNomProvince(String nomProvince) {
+        this.nomProvince = nomProvince;
+    }
+
+    public String getNomPays() {
+        return nomPays;
+    }
+
+    public void setNomPays(String nomPays) {
+        this.nomPays = nomPays;
+    }
 }
---
+++ src/classes/Annuaire.java
@@ -0,0 +1,49 @@
+package classes;
+
+public class Annuaire {
+    private String nom;
+    private String prenom;
+    private String numero;
+    private Adresse adresse;
+
+    public Annuaire(String nom, String prenom, String numero, Adresse adresse){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numero = numero;
+        this.adresse = adresse;
+    }
+
+    /* getters and setters */
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+}
