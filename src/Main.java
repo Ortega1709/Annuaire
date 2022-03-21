b3542c418c71780a7df2c1afa610f3ea5e1f5cd4
kabweortega@gmail.com <kabweortega@gmail.com>
2022-03-21 15:55:17 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import classes.*;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 
 public class Main {
@@ -7,22 +8,14 @@ public class Main {
         // linked list
         LinkedList<Annuaire> annuaires = new LinkedList<>();
 
-        // three object adresse
-        Adresse adresse = new Adresse(33,"Depot","Haut-Katanga","RDC");
-        Adresse adresse1 = new Adresse(22,"Biayi","Haut-Katanga","RDC");
-        Adresse adresse2 = new Adresse(45,"Kambove","Haut-Katanga","RDC");
-
-        // three object annuaire
-        Annuaire annuaire = new Annuaire("Kabwe Mulunda","Ortega","0996875512",adresse);
-        Annuaire annuaire1 = new Annuaire("Kalombo Mutshi","Jonathan","0828547535",adresse1);
-        Annuaire annuaire2 = new Annuaire("Kamba Lamu","Didier","0828585475",adresse2);
-
-        // insertion in linkedList annuaires
-        annuaires.add(0,annuaire);
-        annuaires.add(1,annuaire1);
-        annuaires.add(2,annuaire2);
-
         // show all annuaire in linkedList
+        allAnnuaires(annuaires);
+    }
+
+    // fonction show all annuaire in LinkedList annuaires ...
+    public static void allAnnuaires(LinkedList<Annuaire> annuaires){
+        System.out.println("*****************ANNUAIRES******************\n");
+
         for (int i = 0; i < annuaires.size(); i++) {
             System.out.println("Nom: "+annuaires.get(i).getNom());
             System.out.println("Prenom: "+annuaires.get(i).getPrenom());
@@ -31,7 +24,10 @@ public class Main {
             System.out.println("Avenue: "+annuaires.get(i).getAdresse().getNomAvenue());
             System.out.println("Province: "+annuaires.get(i).getAdresse().getNomProvince());
             System.out.println("Pays: "+annuaires.get(i).getAdresse().getNomPays());
-            System.out.println("\n***********************************************");
+            System.out.println("\n*********************************************");
         }
     }
+
+    // fonction add annuaire in annuaires
+
 }
